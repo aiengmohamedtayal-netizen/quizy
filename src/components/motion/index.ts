6570ce8a4b9d5da58e4add19a036ea60571bb230
mohamedtayal <aiengmohamedtayal@gmail.com>
2026-09-05 22:26:24 +0000

    feat(ui): export new visual motion primitives

--- src/components/motion/index.ts
+++ src/components/motion/index.ts
@@ -3,3 +3,5 @@ export { useTypewriter, type UseTypewriterOptions } from "./useTypewriter";
 export { AnimatedNumber, type AnimatedNumberProps } from "./AnimatedNumber";
 export { TiltCard, type TiltCardProps } from "./TiltCard";
 export { MagneticButton, type MagneticButtonProps } from "./MagneticButton";
+export { FloatingVector, type FloatingVectorProps } from "./FloatingVector";
+export { QuizyHeroVisual } from "./QuizyHeroVisual";
